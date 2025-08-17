b19d5aa050bad586590524a556c185b72fbdc354
SanjaykumarSoy <157505017+SanjaykumarSoy@users.noreply.github.com>
2025-08-17 12:18:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const applyBtn = document.getElementById('applyBtn');
-
+ 
 applyBtn.addEventListener('click', () => {
     window.location.href = "https://youtu.be/dQw4w9WgXcQ?si=Td-7oe4Fhhkv1ab5";
 });
